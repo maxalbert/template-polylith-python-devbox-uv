6b8fdacbc4d9b2d4eb9e1fdc6bf372205e70b9fa
Maximilian Albert <maximilian.albert@gmail.com>
2024-12-09 22:16:11 +0000

    config: add pytest fixture 'anyio_backend'

--- test/conftest.py
+++ test/conftest.py
@@ -3,6 +3,11 @@ import pytest
 from {{ pymodule_name }}.logging import configure_structlog
 
 
+@pytest.fixture
+def anyio_backend() -> str:
+    return "asyncio"
+
+
 @pytest.fixture(scope="session", autouse=True)
 def disable_colored_logging_output() -> None:
     configure_structlog(use_colors=False)
