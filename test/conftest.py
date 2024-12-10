a334a9a69c54009a5c68208c00b6c7cd2af7bb5f
Maximilian Albert <maximilian.albert@gmail.com>
2024-12-10 07:33:38 +0000

    test: new fixture 'test_client' (for Litestar test client)

--- test/conftest.py
+++ test/conftest.py
@@ -1,6 +1,15 @@
+from typing import TYPE_CHECKING
+from collections.abc import AsyncIterator
+
 import pytest
 
+from litestar.testing import AsyncTestClient
+
 from {{ pymodule_name }}.logging import configure_structlog
+from {{ pymodule_name }}.server import app
+
+if TYPE_CHECKING:
+    from litestar import Litestar
 
 
 @pytest.fixture
@@ -11,3 +20,9 @@ def anyio_backend() -> str:
 @pytest.fixture(scope="session", autouse=True)
 def disable_colored_logging_output() -> None:
     configure_structlog(use_colors=False)
+
+
+@pytest.fixture(scope="function")
+async def test_client() -> AsyncIterator[AsyncTestClient["Litestar"]]:
+    async with AsyncTestClient(app=app) as client:
+        yield client
